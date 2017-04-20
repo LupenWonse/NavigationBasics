73c80a1e80f284e47203dd45957a680a5e23c56f
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-04-20 18:35:14 -0400

    Obstacle Generation

---
+++ Assets/ObstacleGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour {
+
+	public GameObject obstacle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray,out hit, 100.0f)){
+				GameObject.Instantiate(obstacle,hit.collider.gameObject.transform.position + Vector3.up*2,Quaternion.identity);
+			}
+		}
+	}
+}
