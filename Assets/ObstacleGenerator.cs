521cfd6373c48e7150d18e19aaa9963c5d02ac66
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-04-21 09:30:00 -0400

    Removing obstacles

--- Assets/ObstacleGenerator.cs
+++ Assets/ObstacleGenerator.cs
@@ -17,8 +17,14 @@ public class ObstacleGenerator : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
-			if(Physics.Raycast(ray,out hit, 100.0f)){
-				GameObject.Instantiate(obstacle,hit.collider.gameObject.transform.position + Vector3.up*2,Quaternion.identity);
+			if(Physics.Raycast(ray,out hit, 100.0f,LayerMask.GetMask(new string[] {"Board"}))){
+				if (hit.transform.childCount == 0){
+					GameObject.Instantiate(obstacle,hit.collider.gameObject.transform.position + Vector3.up*2,Quaternion.identity,hit.transform);
+				} else {
+					Destroy(hit.transform.GetChild(0).gameObject);
+				}
+				
+				
 			}
 		}
 	}
